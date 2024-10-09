1eb90ac29015ad6719b156e5cdafc53923acd897
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2024-10-09 23:43:50 +0900

    feat: Join with social login backend API(#10)

--- src/screens/LoginScreen.tsx
+++ src/screens/LoginScreen.tsx
@@ -1,12 +1,18 @@
-import React from 'react';
-import { View, TextInput, Button, ScrollView, TouchableOpacity, Image, StyleSheet, SafeAreaView, Text, TouchableWithoutFeedback } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Linking, TouchableOpacity, Modal, Dimensions, View, Text, TouchableWithoutFeedback, Image, StyleSheet, SafeAreaView, ActivityIndicator } from 'react-native';
+import { WebView } from 'react-native-webview';
 import { StackNavigationProp } from '@react-navigation/stack';
 import Logo from '../../assets/logo.png';
 import Naver from '../../assets/naver.png';
+import qs from 'qs';
+import axios, {isCancel, AxiosError} from 'axios';
+import EncryptedStorage from 'react-native-encrypted-storage';
+import { jwtDecode } from "jwt-decode";
 
 type RootStackParamList = {
   Login: undefined;
   EmailLogin: undefined;
+  OAuthResponse: { token: string };
 };
 
 type LoginScreenNavigationProp = StackNavigationProp<
@@ -24,14 +30,7 @@ const styles = StyleSheet.create({
     paddingTop: 26,
     flexDirection: 'column',
     alignItems: 'center',
-    backgroundColor: 'white'
-  },
-  text: {
-    fontSize: 20,
-    color: '#252525',
-    paddingHorizontal: 16,
-    fontWeight: '600',
-    marginBottom: 10,
+    backgroundColor: 'white',
   },
   logoText: {
     fontSize: 32,
@@ -59,34 +58,191 @@ const styles = StyleSheet.create({
     fontWeight: '1000',
     color: 'white',
   },
+  alertOverlay: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+  },
+  alertContainer: {
+    width: 300,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 20,
+    alignItems: 'center',
+  },
+  alertTitle: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginBottom: 10,
+  },
+  alertMessage: {
+    fontSize: 14,
+    color: '#8E8E8E',
+    textAlign: 'center',
+    marginBottom: 5,
+  },
+  alertButtonContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+    alertButton: {
+      flex: 1,
+      height: 45,
+      justifyContent: 'center',
+      alignItems: 'center',
+      borderRadius: 10,
+      marginHorizontal: 5,
+    },
+    cancelButton: {
+      backgroundColor: '#252525',
+    },
+    cancelButtonText: {
+      color: 'white',
+      fontWeight: 'bold',
+    },
 });
 
 function LoginScreen({ navigation }: Props) {
-  const handleNaver = () => {
-    console.log('네이버 로그인');
+  const [naverLoginVisible, setNaverLoginVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const deviceHeight = Dimensions.get('window').height;
+  const deviceWidth = Dimensions.get('window').width;
+  const [alertVisible, setAlertVisible] = useState(false);
+
+  const handleNaverLogin = () => {
+    setNaverLoginVisible(true);
   };
-  const handleEmail = () => {
-    navigation.navigate('EmailLogin')
+
+  const handleEmailLogin = () => {
+    navigation.navigate('EmailLogin');
   };
-  return (
+
+  const handleWebViewNavigationStateChange = (navState: any) => {
+    const { url } = navState;
+    console.log("Navigated to:", url);
+
+    // 로그인 성공 시 리다이렉트
+    if (url.startsWith('http://localhost:8080/login/oauth2/code/naver')) {
+      const queryString = url.split('?')[1];
+      const params = qs.parse(queryString);
+
+      const code = params.code;
+      const state = params.state;
+
+      console.log('Authorization Code:', code);
+      console.log('State:', state);
+
+      postLoginCodeAndState(code, state);
+
+      setNaverLoginVisible(false);
+    }
+  };
+
+  //네이버 로그인 post 요청
+  const postLoginCodeAndState = async (code: string, state: string) => {
+    try {
+      const data = {
+        code: code,
+        state: state
+      };
+
+      const response = await axios.post('http://192.168.45.77:8080/auth/oauth2', data);
+
+      if (response.status === 200) {
+        const { accessToken, refreshToken } = response.data;
+        const email = parseEmailFromToken(accessToken);
+
+        if (accessToken && email) {
+          console.log('Access Token:', accessToken);
+          console.log('email:', email);
+        } else {
+          setAlertVisible(true);
+        }
+
+        // 토큰을 저장 (예: Secure Storage나 Encrypted Storage에 저장)
+        await EncryptedStorage.setItem('accessToken', accessToken);
+        await EncryptedStorage.setItem('email', email);
+
+        navigation.navigate('TabNavigation')
+      }
+
+    } catch (error) {
+      console.error('Error during POST request:', error);
+      setAlertVisible(true);
+    }
+  };
+
+  //토큰에서 이메일 파싱
+  const parseEmailFromToken = (accessToken: string) => {
+    try {
+
+      const decoded: any = jwtDecode(accessToken);
+      const email = decoded.username;
+
+      return email;
+    } catch (error) {
+      console.error('Failed to parse email from token:', error);
+      return null;
+    }
+  };
+
+  return naverLoginVisible ? (
+      <WebView source={{uri: 'http://192.168.45.77:8080/oauth2/authorization/naver'}}
+        style={{flex: 1}}
+        onNavigationStateChange={handleWebViewNavigationStateChange}
+        startInLoadingState={true}
+        renderLoading={() => <ActivityIndicator size="large" color="#0000ff" />}
+      />
+  ) : (
     <SafeAreaView style={styles.container}>
         <View style={{height: 90}}/>
-        <Image source={Logo} style={{height: 150, width: 150}}/>
-        <View style={styles.container}>
+          <Image source={Logo} style={{ height: 150, width: 150 }} />
+          <View style={styles.container}>
             <Text style={styles.logoText}>VoiceLog</Text>
-            <View style={{height: 150}}/>
-            <TouchableWithoutFeedback onPress={handleNaver}>
+            <View style={{ height: 150 }} />
+            <TouchableWithoutFeedback onPress={handleNaverLogin}>
               <View>
-                <Image source={Naver} style={styles.buttonImage}/>
+                <Image source={Naver} style={styles.buttonImage} />
               </View>
             </TouchableWithoutFeedback>
-            <View style={{height: 20}}/>
-            <TouchableWithoutFeedback onPress={handleEmail}>
+            <View style={{ height: 20 }} />
+            <TouchableWithoutFeedback onPress={handleEmailLogin}>
               <View style={styles.button}>
                 <Text style={styles.buttonText}>이메일로 로그인</Text>
               </View>
             </TouchableWithoutFeedback>
-        </View>
+          </View>
+
+          <Modal
+              animationType="slide"
+              transparent={true}
+              visible={alertVisible}
+              onRequestClose={() => setAlertVisible(false)}
+              style={{ justifyContent: 'center', alignItems: 'center', margin: 0 }}
+            >
+              <View style={styles.alertOverlay}>
+                <View style={styles.alertContainer}>
+                  <Text style={styles.alertTitle}>로그인 실패</Text>
+                  <Text style={styles.alertMessage}>로그인에 실패하였습니다.</Text>
+                  <View style={{height: 15}}/>
+                  <View style={styles.alertButtonContainer}>
+                    <TouchableOpacity
+                      style={[styles.alertButton, styles.cancelButton]}
+                      onPress={() => setAlertVisible(false)}
+                    >
+                      <Text style={styles.cancelButtonText}>확인</Text>
+                    </TouchableOpacity>
+                  </View>
+                </View>
+              </View>
+            </Modal>
     </SafeAreaView>
   );
 }
